16c2e7e0bec18a419dbd50eb7a85e25d3d19022d
Tomoe Yokotani <tomoe_yokotani@anycarry.co.jp>
2025-11-07 13:21:43 +0900

    ベンチマークテスト実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
- # ページごとの完全なタイトルを返す
-  def full_title(page_title = '')
+ # ページごとの完全なタイトルを返す関数を作成
+  def full_title(page_title = '')#引数を取るけど、何もなければ空文字をデフォルトにする
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
