2d77e8af3b22a731a642bcfdf5da3bcb13e2457d
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-08 21:51:34 +0900

    signupページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/helf', to: 'static_pages#helf'
+  get '/signup',to: 'users#new'
   # get 'login',to: 'static_pages#login'
   # get 'static_pages/home'
   # get 'static_pages/help'
