600685a97d9f55f62ae592bc42e7489ece8d53c7
Tomoe Yokotani <tomoe_yokotani@anycarry.co.jp>
2025-11-08 17:43:42 +0900

    リンクURLの書き方リファクタ

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  
   root 'static_pages#home'
+  get '/help', to: 'static_pages#help' #,as:'helf' asでリンク名変更が可能！
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/helf', to: 'static_pages#helf'
+  # get 'login',to: 'static_pages#login'
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
 end
