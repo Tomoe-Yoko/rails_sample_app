bec438c7d97956d5d507b13796aaf5b82d81fdda
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-12 16:26:52 +0900

    10章実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: %i[index edit update] # 各アクションの前に実行される [:index, :edit, :update]
+  before_action :logged_in_user, only: %i[index edit update destroy] # 各アクションの前に実行される [:index, :edit, :update]
   before_action :correct_user,   only: %i[edit update]
-
+before_action :admin_user,     only: :destroy
   def index
-    @users = User.all
+    # @users = User.all
+    @users = User.paginate(page: params[:page])
   end
   def show
     @user = User.find(params[:id])
@@ -41,6 +42,14 @@ class UsersController < ApplicationController
     end
   end
 
+ def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url, status: :see_other
+  end
+
+
+
   private
 
   def user_params
@@ -63,4 +72,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
+
+  # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url, status: :see_other) unless current_user.admin?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_11_11_064717) do
+ActiveRecord::Schema[7.0].define(version: 2025_11_12_055809) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_11_11_064717) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
