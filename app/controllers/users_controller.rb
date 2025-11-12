a6b772ea8b744b3f6a028b4c07c820c6d2498672
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-12 23:48:47 +0900

    ユーザーのcreateアクションを更新

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[index edit update destroy] # 各アクションの前に実行される [:index, :edit, :update]
   before_action :correct_user,   only: %i[edit update]
-before_action :admin_user,     only: :destroy
+  before_action :admin_user, only: :destroy
   def index
     # @users = User.all
     @users = User.paginate(page: params[:page])
   end
+
   def show
     @user = User.find(params[:id])
     #  debugger
@@ -19,10 +20,9 @@ before_action :admin_user,     only: :destroy
   def create
     @user = User.new(user_params) # 実装は終わっていないことに注意!
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = 'Welcome to the Sample App!'
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
@@ -42,13 +42,11 @@ before_action :admin_user,     only: :destroy
     end
   end
 
- def destroy
+  def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url, status: :see_other
   end
-
-
 
   private
 
@@ -60,11 +58,11 @@ before_action :admin_user,     only: :destroy
 
   # ログイン済みユーザーかどうか確認
   def logged_in_user
-      unless logged_in?
-        store_location # アクセスしようとしたURLを保存
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url, status: :see_other
-    end
+    return if logged_in?
+
+    store_location # アクセスしようとしたURLを保存
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, status: :see_other
   end
 
   # 正しいユーザーかどうか確認
@@ -74,7 +72,7 @@ before_action :admin_user,     only: :destroy
   end
 
   # 管理者かどうか確認
-    def admin_user
-      redirect_to(root_url, status: :see_other) unless current_user.admin?
-    end
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
 end
