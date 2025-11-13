d8266ecaa651334ead25dc1a1516198b751034d4
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-13 09:35:01 +0900

    ユーザーの有効化テストを分割する

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,8 +5,10 @@ require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+  parallelize(workers: 1)
+
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors, with: :threads)
+  # parallelize(workers: :number_of_processors, with: :threads)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
