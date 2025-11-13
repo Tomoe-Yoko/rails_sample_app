750af62c53f599d4d3db3e9cd3cd04f533233b9c
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-13 23:29:32 +0900

    UserとMicropostの関連付けを改良

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }#Procやlambda（もしくは無名関数）と呼ばれるオブジェクトを作成する文法 scope :recent, order(created_at: :desc)
   validates:user_id,presence:true
   validates :content, presence: true, length: { maximum: 255 }
 end
