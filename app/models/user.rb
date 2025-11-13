bf8c0acc8a9d624dbb9161a5d0eddcb75458ec4c
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-13 13:23:40 +0900

    パスワード再設定のメール送信実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token # 仮想属性（attr_accessorで定義）なので、DBには保存されない
+  attr_accessor :remember_token, :activation_token , :reset_token# 仮想属性（attr_accessorで定義）なので、DBには保存されない
 
   before_save   :downcase_email
   before_create :create_activation_digest
@@ -62,6 +62,19 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
 
   private
 
