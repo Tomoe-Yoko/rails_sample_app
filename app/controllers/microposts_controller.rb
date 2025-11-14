094c8137e53b58d20568c301bfd69d85e2f43e58
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-14 11:02:11 +0900

    Micropost の削除

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,8 @@
 class MicropostsController < ApplicationController
+  def destroy
+    @micropost=current_user.microposts.find(params[:id])
+    @micropost.destroy
+    flash[:success]="Micropost deleted"
+    redirect_to request.referrer || root_url
+  end
 end
