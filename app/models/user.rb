eda5c79e1a819817bc5c262b8a00e3278c9f8057
Tomoe Yokotani <osmanthus31172@gmail.com>
2025-11-17 10:38:46 +0900

    フォロー／アンフォローのrouteコントローラ作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,16 @@ class User < ApplicationRecord
 
   # 14chapter フォロー機能
   # 自分 → 他人
-  has_many :active_relationships,
-           class_name: 'Relationship',
-           foreign_key: 'follower_id',
-           dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed # Rubyで user.following と書くだけでフォロー相手一覧が取れる
-  # 他人 → 自分
-  has_many :passive_relationships,
-           class_name: 'Relationship',
-           foreign_key: 'followed_id',
-           dependent: :destroy
+
+  has_many :active_relationships, class_name: 'Relationship',
+                                  foreign_key: 'follower_id',
+                                  dependent: :destroy
+
+  has_many :passive_relationships, class_name: 'Relationship',
+                                   foreign_key: 'followed_id',
+                                   dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
   # 8章渡された文字列のハッシュ値を返す
@@ -95,7 +95,18 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+ # 14章 フォロー機能のメソッド
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
 
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id)&.destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
   private
 
   # メールアドレスをすべて小文字にする
@@ -109,17 +120,5 @@ class User < ApplicationRecord
     self.activation_digest = User.digest(activation_token)
   end
 
-  # 14章 フォロー機能のメソッド
-  def follow(other_user)
-    active_relationships.create(followed_id: other_user.id)
-  end
-
-  def unfollow(other_user)
-    active_relationships.find_by(followed_id:other_user.id)&.destroy
-  end
-
-  def following?(other_user)
-    following.include?(other_user)
-  end
-  
+ 
 end
